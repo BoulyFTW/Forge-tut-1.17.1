441043553cedbd8d4e9720fcc342cf45a108f1da
BoulyFTW <sharbelaboud10@gmail.com>
2021-12-15 17:41:37 +0200

    Added a creative mod tab

--- src/main/java/net/BoulyFTW/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/BoulyFTW/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.BoulyFTW.tutorialmod.block;
 
 import net.BoulyFTW.tutorialmod.TutorialMod;
+import net.BoulyFTW.tutorialmod.item.ModCreativeModeTab;
 import net.BoulyFTW.tutorialmod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -20,20 +21,33 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
 public static final RegistryObject<Block> TITANIUM_BLOCK = registerBlock("titanium_block",
-        () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f)));
+        () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f)), CreativeModeTab.TAB_MATERIALS);
 
     public static final RegistryObject<Block> TITANIUM_ORE = registerBlock("titanium_ore",
-            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(3f)));
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(3f)));
+
+    private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
+
+
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
-registerBlockItem(name, toReturn);
+        registerBlockItem(name, toReturn);
         return toReturn;
     }
 
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+                new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
     }
 
     public static void  register(IEventBus eventBus) {
